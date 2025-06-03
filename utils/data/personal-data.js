594d74fb85e6e77c9a95c9a8b7865ca90aa1d591
TAQI EDDINE EL MAMOUNI <taqi.mamouni.mdbf21@iste.edu.tr>
2025-06-03 15:42:45 +0300

    save

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "TAQIEDDINE ELMAMOUNI",
   profile: '/profile.jpg',
-  designation: "COMPUTER ENGENIRING",
+  designation: "COMPUTER ENGINEERING",
   description: "My name is TAQI EDDINE EL MAMOUNI, a passionate and professional programmer with a strong focus on Artificial Intelligence, Machine Learning, and Deep Learning. I actively work on projects involving neural networks, computer vision, and natural language processing, driven by a deep curiosity to explore how intelligent systems can solve real-world problems. I thrive in dynamic, collaborative environments where creativity meets technology, and I’m always eager to learn and adopt new tools and frameworks. Alongside my AI interests, I have experience in JavaScript and modern web development. My core expertise lies in backend development using Java script, which remain my favorite technologies to build robust and scalable applications.",
   email: 'taqieddineelmamouni@gmail.com',
   phone: '+90 5013270987',
